e087d60562758af7aeb1ecf0b6e3df120211cfb5
vinai <vinaibangala2001@gmail.com>
2020-02-02 23:33:50 +0530

    modifications

---
+++ pgm/mm/cmd_line.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int add (int x, int y)
+{
+	int z = 10;
+
+	z = x + y;
+
+	return z;
+}
+
+
+int main(int argc, char **argv)
+{
+	int a = atoi(argv[1]);
+	int b = atoi(argv[2]);
+
+	int c;
+
+	char buffer[100];
+
+
+	gets(buffer);
+	puts(buffer);
+
+	c = add(a,b);
+
+	printf("sum of %d + %d = %d\n", a, b, c);
+
+	return 0;
+}
+
